caeccbd11c7b1e3872e98f2c5d27a293c7ce7f0e
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-08-08 21:21:58 +0500

    React-Landing-Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState } from 'react';
 
-// style file link
-import '../style/Navbar.css'
+// style file import 
+import '../style/navbar.css';
 
-function Navbar() {
-  return (
-    <>
-      <nav>
-        <h2>Landing Page</h2>
-        <div>
-          <ul id="navbar">
-            <li><a href="#" className="active">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </div>
-        <div id="mobile">
-          <i id="bar"></i>
-        </div>
-      </nav>
-    </>
-  );
-}
+// Functional component
+const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
+    return (
+        <>
+            <nav>
+                <h2>Landing Page</h2>
+                <div>
+                    <ul id='navbar' className={clicked ? "#navbar active" : "#navbar"}>
+                        <li><a href="#" className='active'>Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div id="mobile" onClick={handleClick}>
+                    <i id='bar' className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+            </nav>
+        </>
+    );
+};
 
 export default Navbar;
