8a549a76cd0148d33cc5defe1763f5a916c92a94
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-08-09 14:43:43 +0500

    React-Landing-Page

---
+++ src/pages/FeaturesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+function FeaturesPage() {
+  return (
+    <>
+        <Navbar />
+    </>
+  )
+}
+
+export default FeaturesPage;
